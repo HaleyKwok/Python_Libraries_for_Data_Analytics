cdd7c5eae112e67f3b991405a3365865851a7be0
KHC <98343091+HaleyKwok@users.noreply.github.com>
2022-08-09 18:41:34 +0800

    Update more_indexing&slicing.py

--- NumPy/more_indexing&slicing.py
+++ NumPy/more_indexing&slicing.py
@@ -18,3 +18,15 @@ cols = np.array([[0,2],[0,2]])
 y = x[rows,cols]  
 print  ('这个数组的四个角元素是：')
 print (y)
+
+print('----------------------------')
+
+import numpy as np
+ 
+a = np.array([[1,2,3], [4,5,6],[7,8,9]])
+b = a[1:3, 1:3]
+c = a[1:3,[1,2]]
+d = a[...,1:]
+print(b)
+print(c)
+print(d)
