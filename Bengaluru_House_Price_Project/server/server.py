dc0dcce9593db0e4fa94e6d996b080b2bf13a41e
KHC <98343091+HaleyKwok@users.noreply.github.com>
2022-08-15 03:58:14 +0800

    files

--- Bengaluru_House_Price_Project/server/server.py
+++ Bengaluru_House_Price_Project/server/server.py
@@ -1,1 +1,10 @@
-from flask
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    print("Start Python Flask Server For Banglore Home Price Prediction.")
+    app.run()
