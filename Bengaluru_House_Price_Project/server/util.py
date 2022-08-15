3faee59c396bce28e98d65a6b987ca88f0486f5b
KHC <98343091+HaleyKwok@users.noreply.github.com>
2022-08-15 17:41:36 +0800

    update files

--- Bengaluru_House_Price_Project/server/util.py
+++ Bengaluru_House_Price_Project/server/util.py
@@ -1,6 +1,28 @@
+import json 
+import pickle 
+
+# global variables
+__locations = None
+__data_columns = None
+__model = None
+
 def get_location_names():
-    pass
+    return __locations
 
+def load_saved_artifacts():
+    print("loading saved artifacts...\n")
+    global __data_columns
+    global __locations
 
-if __name__ == 'main':
+    # open the file
+    with open("./artifacts/columns.json", "r") as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:] # first 3 columns are "total_sqft", "bath", "bhk", and [3:] starts with "location" 
+
+    with open("./artifacts/banglore_home_prices_model.pickle", "rb") as f:
+        __model = pickle.load(f)
+    print("loading saved artifacts...done\n")
+
+if __name__ == '__main__':
+    load_saved_artifacts()
     print(get_location_names())
