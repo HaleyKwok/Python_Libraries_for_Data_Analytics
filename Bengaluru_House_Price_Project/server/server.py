2d5cad58dcae43a9e7d57c968a6b6b8c0d784046
KHC <98343091+HaleyKwok@users.noreply.github.com>
2022-08-15 16:59:37 +0800

    Update server.py

--- Bengaluru_House_Price_Project/server/server.py
+++ Bengaluru_House_Price_Project/server/server.py
@@ -1,9 +1,16 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
+@app.route('/get_location_names')
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+
+    return response
     return "Hello World!"
+
+
 
 if __name__ ==  "__main__":
     print("Start Python Flask Server For Banglore Home Price Prediction.")
