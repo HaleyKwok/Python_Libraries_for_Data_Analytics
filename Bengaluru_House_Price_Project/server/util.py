4881800d034f3f4ff1693904210d7a27e76a0db5
KHC <98343091+HaleyKwok@users.noreply.github.com>
2022-08-15 20:55:10 +0800

    Update util.py

--- Bengaluru_House_Price_Project/server/util.py
+++ Bengaluru_House_Price_Project/server/util.py
@@ -9,11 +9,11 @@ __model = None
 
 def get_estimated_price(location, sqft, bhk, bath):
     try:
-        loc_index = __data_columns.index(location.lower())
+        loc_index = __data_columns.index(location.lower()) 
     except:
-        loc_index = -1
+        loc_index = -1 # if location is not found, return -1
          
-    x = np.zeros(len(X.columns))
+    x = np.zeros(len(__data_columns)) # create a numpy array of zeros of size len(__data_columns)
     x[0] = sqft
     x[1] = bath
     x[2] = bhk
@@ -21,6 +21,21 @@ def get_estimated_price(location, sqft, bhk, bath):
         x[loc_index] = 1
     return __model.predict([x])
 
+'''
+
+predict price model from Project.ipynb
+def predict_price(location, sqft, bhk, bath):
+    loc_index = np.where(X.columns == location)[0][0] 
+
+    x = np.zeros(len(X.columns))
+    x[0] = sqft
+    x[1] = bhk
+    x[2] = bath
+    if loc_index >= 0:
+        x[loc_index] = 1 
+    return lr.predict([x])[0]
+
+'''
 
 def get_location_names():
     return __locations
