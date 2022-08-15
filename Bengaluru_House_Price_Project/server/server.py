ea456d6ebd5de51434f3406cbc4ef6a364d55327
KHC <98343091+HaleyKwok@users.noreply.github.com>
2022-08-15 16:37:25 +0800

    Update server.py

--- Bengaluru_House_Price_Project/server/server.py
+++ Bengaluru_House_Price_Project/server/server.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-if __name__ == "__main__":
+if __name__ ==  "__main__":
     print("Start Python Flask Server For Banglore Home Price Prediction.")
     app.run()
