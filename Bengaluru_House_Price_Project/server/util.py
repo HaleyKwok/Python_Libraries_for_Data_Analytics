649dfc62fae611a56e541d888ae7fa028ab0e0e3
KHC <98343091+HaleyKwok@users.noreply.github.com>
2022-08-15 20:51:54 +0800

    Update util.py

--- Bengaluru_House_Price_Project/server/util.py
+++ Bengaluru_House_Price_Project/server/util.py
@@ -1,10 +1,26 @@
 import json 
 import pickle 
+import numpy as np
 
 # global variables
 __locations = None
 __data_columns = None
 __model = None
+
+def get_estimated_price(location, sqft, bhk, bath):
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+         
+    x = np.zeros(len(X.columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index >= 0:
+        x[loc_index] = 1
+    return __model.predict([x])
+
 
 def get_location_names():
     return __locations
