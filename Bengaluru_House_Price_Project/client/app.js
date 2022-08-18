31b57d273e19ff53eb4bcb7373d959308b98e32f
KHC <98343091+HaleyKwok@users.noreply.github.com>
2022-08-18 12:29:08 +0800

    Update app.js

--- Bengaluru_House_Price_Project/client/app.js
+++ Bengaluru_House_Price_Project/client/app.js
@@ -1,24 +1,62 @@
 
-function 
+function getBHKValue() {
+var uiBHK = document.getElementsByName("uiBHK");
+for(var i in uiBHK) {
+    if(uiBHK[i].checked) {
+        return parseInt(i)+1;
+    }
+}
+return -1; // Invalid Value
+}
 
-
-
-
+function getBathValue() {
+    var uiBathrooms = document.getElementsByName("uiBathrooms");
+    for(var i in uiBathrooms) {
+      if(uiBathrooms[i].checked) {
+          return parseInt(i)+1;
+      }
+    }
+    return -1; // Invalid Value
+  }
+  
+// got the reaction based on the price estimation button 
+function onClickedEstimatePrice() {
+    console.log("Estimate price button clicked");
+    var sqft = document.getElementById("uiSqft");
+    var bhk = getBHKValue(); // return the function BHK value
+    var bathrooms = getBathValue(); // return the function Bathrooms
+    var location = document.getElementById("uiLocations");
+    var estPrice = document.getElementById("uiEstimatedPrice");
+  
+    var url = "http://127.0.0.1:5000/predict_home_price"; // for postman
+    var url = "/api/predict_home_price"; // for nginx
+  
+    $.post(url, {
+        total_sqft: parseFloat(sqft.value),
+        bhk: bhk,
+        bath: bathrooms,
+        location: location.value
+    },function(data, status) {
+        console.log(data.estimated_price);
+        estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
+        console.log(status);
+    });
+  }
 
 
 
 // need location information from the backend
 function onPageLoad() {
     console.log( "document loaded" );
-    var url = "http://127.0.0.1:5000/get_location_names"; // get the location names from the backend
+    var url = "http://127.0.0.1:5000/get_location_names"; // for postman: get the location names from the backend
     // var url = "/api/get_location_names"; // for nginx
-    $.get(url,function(data, status) {
+    $.get(url,function(data, status) { // get the data from the url 
         console.log("got response for get_location_names request");
         if(data) {
             var locations = data.locations;
             var uiLocations = document.getElementById("uiLocations");
             $('#uiLocations').empty();
-            for(var i in locations) {
+            for(var i in locations) {  // add the locatio one by one to the dropdown
                 var opt = new Option(locations[i]);
                 $('#uiLocations').append(opt);
             }
