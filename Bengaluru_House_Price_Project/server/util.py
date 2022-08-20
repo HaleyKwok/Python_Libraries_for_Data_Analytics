491d7d6c73ede8194d58ddf8cf25fb0b1f6394aa
KHC <98343091+HaleyKwok@users.noreply.github.com>
2022-08-20 21:52:19 +0800

    Update util.py

--- Bengaluru_House_Price_Project/server/util.py
+++ Bengaluru_House_Price_Project/server/util.py
@@ -69,6 +69,7 @@ def get_location_names():
 if __name__ == '__main__':
     load_saved_artifacts()
     print(get_location_names())
+    # for testing
     print(get_estimated_price('1st Phase JP Nagar', 1000, 3, 3))
     print(get_estimated_price('1st Phase JP Nagar', 1000, 3, 2))
     print(get_estimated_price('Kalhalli', 1000, 3, 2)) # other location
